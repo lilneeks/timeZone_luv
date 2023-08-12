afd18b86008b1bc8bfce678a3c925b9b59521c31
Nicko <nickolearns2code@gmail.com>
2023-08-11 20:36:02 -0700

    working ver

--- js/main.js
+++ js/main.js
@@ -1,155 +1,79 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const startBtn = document.getElementById('startBtn');
-    const newGameBtn = document.getElementById('newGameBtn');
-    const hitBtn = document.getElementById('hitBtn');
-    const standBtn = document.getElementById('standBtn');
-    const overlay = document.getElementById('overlay');
-    const winsTracker = document.getElementById('winsTracker');
-
-    let playerCards, dealerCards, playerScore, dealerScore, deck, gameInProgress, playerTurn;
-    let playerWins = 0;
-    let dealerWins = 0;
-
-    function createDeck() {
-        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-        return suits.flatMap(suit => values.map(value => ({ suit, value })));
+document.addEventListener('DOMContentLoaded', function() {
+    function updateTime(timeZone, elementId) {
+        const options = { timeZone: timeZone, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        const currentTime = new Date().toLocaleTimeString('en-US', options);
+        document.getElementById(elementId).textContent = currentTime;
     }
 
-    function getRandomIndex(max) {
-        return Math.floor(Math.random() * max);
+    function updateTimes() {
+        updateTime('America/Los_Angeles', 'nicko-time');
+        updateTime('Asia/Seoul', 'sunyoung-time');
     }
 
-    function shuffle(deck) {
-        deck.forEach((_, i) => [deck[i], deck[getRandomIndex(deck.length)]] = [deck[getRandomIndex(deck.length)], deck[i]]);
+    function updateColors() {
+        const nickoZone = document.querySelector('.time-container:nth-child(1) .time-zone');
+        const sunyoungZone = document.querySelector('.time-container:nth-child(2) .time-zone');
+        
+        const currentTime = new Date();
+        const nickoColor = `hsl(${currentTime.getSeconds() * 15}, 90%, 50%)`;
+
+        nickoZone.style.color = nickoColor;
+        sunyoungZone.style.color = 'purple';
     }
 
-    function dealCards() {
-        return [deck.pop(), deck.pop()];
+    let simulationStartTime = null;
+
+    function simulateDayCycle() {
+        simulationStartTime = Date.now();
+        requestAnimationFrame(stepSimulation);
     }
 
-    function showCards(cards, player) {
-        const cardsElement = document.getElementById(player + '-cards');
-        cardsElement.innerHTML = cards.map(card => `<div>${card.value} of ${card.suit}</div>`).join('');
-    }
+    function stepSimulation() {
+        const elapsedTime = Date.now() - simulationStartTime;
+        const totalSimulationTime = 6000; // 6 seconds for a faster simulation
 
-    function getScore(cards) {
-        const hasAce = cards.some(card => card.value === 'A');
-        let score = cards.reduce((sum, card) => sum + getCardValue(card.value), 0);
-        return hasAce && score + 10 <= 21 ? score + 10 : score;
-    }
+        if (elapsedTime < totalSimulationTime) {
+            const simulatedTime = (elapsedTime / totalSimulationTime) * 86400;
+            const nickoHour = Math.floor(simulatedTime / 3600);
+            const nickoMinute = Math.floor((simulatedTime - (nickoHour * 3600)) / 60);
 
-    function getCardValue(value) {
-        return ['K', 'Q', 'J'].includes(value) ? 10 : value === 'A' ? 1 : parseInt(value);
-    }
-
-    function updateScore() {
-        playerScore = getScore(playerCards);
-        dealerScore = getScore(dealerCards);
-        document.getElementById('player-score').textContent = `Player Score: ${playerScore}`;
-        document.getElementById('dealer-score').textContent = `Dealer Score: ${dealerScore}`;
-    }
-
-    function startGame() {
-        if (gameInProgress) return;
-        gameInProgress = true;
-        startBtn.disabled = newGameBtn.disabled = true;
-        hitBtn.disabled = standBtn.disabled = false;
-        playerTurn = true;
-        deck = createDeck();
-        shuffle(deck);
-        playerCards = dealCards();
-        dealerCards = dealCards();
-        showCards(playerCards, 'player');
-        showCards(dealerCards, 'dealer');
-        updateScore();
-        document.getElementById('message').textContent = 'Your Turn. Hit or Stand?';
-    }
-
-    function hit() {
-        if (!gameInProgress || !playerTurn) return;
-        playerCards.push(deck.pop());
-        showCards(playerCards, 'player');
-        updateScore();
-
-        if (playerScore > 21) {
-            document.getElementById('message').textContent = 'Busted! Dealer wins.';
-            disableButtonsAfterBust();
-            showTryAgainOverlay();
-        } else if (playerScore === 21) {
-            stand();
+            updateBackgroundColorForMinute(nickoHour, nickoMinute);
+            requestAnimationFrame(stepSimulation);
+        } else {
+            updateBackgroundColor();
         }
     }
 
-    function stand() {
-        if (!gameInProgress || !playerTurn) return;
-        playerTurn = false;
-        hitBtn.disabled = standBtn.disabled = true;
-        document.getElementById('message').textContent = 'Dealer\'s Turn';
-
-        setTimeout(() => {
-            while (dealerScore < 17) {
-                dealerCards.push(deck.pop());
-                showCards(dealerCards, 'dealer');
-                updateScore();
-            }
-
-            let winnerMessage = '';
-            if (dealerScore > 21 || playerScore > dealerScore) {
-                winnerMessage = 'Dealer busts! You win!';
-                playerWins++;
-            } else if (dealerScore > playerScore) {
-                winnerMessage = 'Dealer wins.';
-                dealerWins++;
-            } else {
-                winnerMessage = 'It\'s a tie!';
-            }
-
-            document.getElementById('message').textContent = winnerMessage;
-            endGame();
-            showTryAgainOverlay();
-        }, 1000);
+    function updateBackgroundColor() {
+        const currentTimeNicko = new Date().getHours();
+        updateBackgroundColorForMinute(currentTimeNicko, 0);
     }
 
-    function disableButtonsAfterBust() {
-        hitBtn.disabled
-= standBtn.disabled = true;
-}
+    function updateBackgroundColorForMinute(nickoHour, nickoMinute) {
+        const body = document.body;
+        const currentTimeNicko = new Date(0, 0, 0, nickoHour, nickoMinute);
+        const currentTimeSunyoung = new Date(currentTimeNicko.getTime() + 16 * 60 * 60 * 1000);
 
-function showTryAgainOverlay() {
-    overlay.style.display = 'flex'; // Show the overlay
-    winsTracker.textContent = `Wins: Player: ${playerWins}, Dealer: ${dealerWins}`;
-}
+        let nickoLuminance = 0;
+        let sunyoungLuminance = 0;
 
-function endGame() {
-    gameInProgress = false;
-    newGameBtn.disabled = false;
-}
+        if(currentTimeNicko.getHours() >= 6 && currentTimeNicko.getHours() <= 18) {
+            nickoLuminance = ((currentTimeNicko.getHours() - 6) / 12) * 100;
+        }
+        if(currentTimeSunyoung.getHours() >= 6 && currentTimeSunyoung.getHours() <= 18) {
+            sunyoungLuminance = ((currentTimeSunyoung.getHours() - 6) / 12) * 100 + 5; // Adding a 5% difference for variance
+        }
 
-function newGame() {
-    overlay.style.display = 'none'; // Hide the overlay
-    startGame();
-}
+        const nickoColor = `hsl(60, 100%, ${nickoLuminance}%)`;
+        const sunyoungColor = `hsl(60, 100%, ${sunyoungLuminance}%)`; 
 
-// Listen for keyboard events
-document.addEventListener('keydown', (event) => {
-    if (!gameInProgress) return;
-
-    const key = event.key.toLowerCase();
-    if (key === 'h') {
-        hit();
-    } else if (key === 's') {
-        stand();
+        body.style.backgroundImage = `linear-gradient(to bottom, ${nickoColor} 50%, ${sunyoungColor} 50%)`;
     }
-});
 
-// Button click events
-startBtn.addEventListener('click', startGame);
-newGameBtn.addEventListener('click', newGame);
-hitBtn.addEventListener('click', hit);
-standBtn.addEventListener('click', stand);
-document.getElementById('try-again-button').addEventListener('click', () => {
-    overlay.style.display = 'none';
-    newGame();
-});
+    // Initial and periodic updates
+    updateTimes();
+    setInterval(updateTimes, 1000);
+    updateColors();
+    setInterval(updateColors, 500);
+    setTimeout(simulateDayCycle, 100); // Delay to allow initial black background to render
 });
