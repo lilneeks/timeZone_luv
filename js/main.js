49e036879f55b42788b90c01fc5455587c1c2470
Nicko <nickolearns2code@gmail.com>
2023-08-12 02:47:49 -0700

    rehauled everything and fixed font not changing color

--- js/main.js
+++ js/main.js
@@ -1,4 +1,62 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const moon = document.querySelector('.moon');
+    const sun = document.querySelector('.sun');
+    const earth = document.querySelector('.earth');
+    let moonDegree = 0;
+    let sunDegree = 0;
+    let earthRotation = 0;
+
+    function createRandomStars() {
+        const starCount = 200;  
+        const starSize = 1;     
+        const earthRect = document.querySelector('.earth').getBoundingClientRect();
+        
+        for (let i = 0; i < starCount; i++) {
+            const x = Math.random() * window.innerWidth;
+            const y = Math.random() * window.innerHeight;
+            
+            // Skip stars inside the Earth
+            if (x >= earthRect.left && x <= earthRect.right && y >= earthRect.top && y <= earthRect.bottom) continue;
+            
+            // Skip stars near the text (you can adjust the 50 value as per your requirement)
+            if (y <= 100 || (window.innerHeight - y) <= 100) continue;
+    
+            const starElement = document.createElement("div");
+            starElement.style.position = "absolute";
+            starElement.style.top = y + "px";
+            starElement.style.left = x + "px";
+            starElement.style.width = starSize + "px";
+            starElement.style.height = starSize + "px";
+            starElement.style.backgroundColor = "white";
+            starElement.style.borderRadius = "50%";
+            document.body.appendChild(starElement);
+        }
+    }
+    
+    
+    createRandomStars();  // Create stars immediately after defining the function
+
+    function createShootingStar() {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+    
+        const shootingStar = document.createElement("div");
+        shootingStar.style.left = x + "px";
+        shootingStar.style.top = y + "px";
+        shootingStar.classList.add("shooting-star");
+    
+        document.body.appendChild(shootingStar);
+    
+        setTimeout(() => {
+            document.body.removeChild(shootingStar);
+        }, 1500); // remove the shooting star after the animation completes
+    
+        setTimeout(createShootingStar, Math.random() * 5000 + 2000); // Create a new shooting star every 2 to 7 seconds
+    }
+    
+    createShootingStar();
+    
+    
     function updateTime(timeZone, elementId) {
         const options = { timeZone: timeZone, hour: 'numeric', minute: 'numeric', second: 'numeric' };
         const currentTime = new Date().toLocaleTimeString('en-US', options);
@@ -10,70 +68,20 @@ document.addEventListener('DOMContentLoaded', function() {
         updateTime('Asia/Seoul', 'sunyoung-time');
     }
 
-    function updateColors() {
-        const nickoZone = document.querySelector('.time-container:nth-child(1) .time-zone');
-        const sunyoungZone = document.querySelector('.time-container:nth-child(2) .time-zone');
-        
-        const currentTime = new Date();
-        const nickoColor = `hsl(${currentTime.getSeconds() * 15}, 90%, 50%)`;
+    function animateCelestialBodies() {
+        moonDegree += 0.012;
+        sunDegree += 0.0023;
+        earthRotation += 0.005;
 
-        nickoZone.style.color = nickoColor;
-        sunyoungZone.style.color = 'purple';
-    }
+        moon.style.transform = `translateY(-50%) rotate(${moonDegree}deg) translateX(200px) rotate(-${moonDegree}deg)`;
+        sun.style.transform = `translateY(-50%) rotate(${sunDegree}deg) translateX(350px) rotate(-${sunDegree}deg)`;
+        earth.style.backgroundImage = `radial-gradient(circle at ${earthRotation%100}% 20%, #fff, #0077D9 20%)`;
 
-    let simulationStartTime = null;
-
-    function simulateDayCycle() {
-        simulationStartTime = Date.now();
-        requestAnimationFrame(stepSimulation);
-    }
-
-    function stepSimulation() {
-        const elapsedTime = Date.now() - simulationStartTime;
-        const totalSimulationTime = 6000; // 6 seconds for a faster simulation
-
-        if (elapsedTime < totalSimulationTime) {
-            const simulatedTime = (elapsedTime / totalSimulationTime) * 86400;
-            const nickoHour = Math.floor(simulatedTime / 3600);
-            const nickoMinute = Math.floor((simulatedTime - (nickoHour * 3600)) / 60);
-
-            updateBackgroundColorForMinute(nickoHour, nickoMinute);
-            requestAnimationFrame(stepSimulation);
-        } else {
-            updateBackgroundColor();
-        }
-    }
-
-    function updateBackgroundColor() {
-        const currentTimeNicko = new Date().getHours();
-        updateBackgroundColorForMinute(currentTimeNicko, 0);
-    }
-
-    function updateBackgroundColorForMinute(nickoHour, nickoMinute) {
-        const body = document.body;
-        const currentTimeNicko = new Date(0, 0, 0, nickoHour, nickoMinute);
-        const currentTimeSunyoung = new Date(currentTimeNicko.getTime() + 16 * 60 * 60 * 1000);
-
-        let nickoLuminance = 0;
-        let sunyoungLuminance = 0;
-
-        if(currentTimeNicko.getHours() >= 6 && currentTimeNicko.getHours() <= 18) {
-            nickoLuminance = ((currentTimeNicko.getHours() - 6) / 12) * 100;
-        }
-        if(currentTimeSunyoung.getHours() >= 6 && currentTimeSunyoung.getHours() <= 18) {
-            sunyoungLuminance = ((currentTimeSunyoung.getHours() - 6) / 12) * 100 + 5; // Adding a 5% difference for variance
-        }
-
-        const nickoColor = `hsl(60, 100%, ${nickoLuminance}%)`;
-        const sunyoungColor = `hsl(60, 100%, ${sunyoungLuminance}%)`; 
-
-        body.style.backgroundImage = `linear-gradient(to bottom, ${nickoColor} 50%, ${sunyoungColor} 50%)`;
+        requestAnimationFrame(animateCelestialBodies); 
     }
 
     // Initial and periodic updates
     updateTimes();
     setInterval(updateTimes, 1000);
-    updateColors();
-    setInterval(updateColors, 500);
-    setTimeout(simulateDayCycle, 100); // Delay to allow initial black background to render
+    requestAnimationFrame(animateCelestialBodies); 
 });
