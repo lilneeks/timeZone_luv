0b89143b76853ad9407eef89f5bcdfd34b3bf043
Nicko <nickolearns2code@gmail.com>
2023-08-12 15:56:28 -0700

    removed redudant styling

--- js/main.js
+++ js/main.js
@@ -7,20 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
     let earthRotation = 0;
 
     function createRandomStars() {
-        const starCount = 200;  
-        const starSize = 1;     
-        const earthRect = document.querySelector('.earth').getBoundingClientRect();
-        
+        const starCount = 200;
+        const starSize = 1;
+        const earthRect = earth.getBoundingClientRect();
+        const fragment = document.createDocumentFragment();
+
         for (let i = 0; i < starCount; i++) {
             const x = Math.random() * window.innerWidth;
             const y = Math.random() * window.innerHeight;
-            
-            // Skip stars inside the Earth
+
             if (x >= earthRect.left && x <= earthRect.right && y >= earthRect.top && y <= earthRect.bottom) continue;
-            
-            // Skip stars near the text (you can adjust the 50 value as per your requirement)
             if (y <= 100 || (window.innerHeight - y) <= 100) continue;
-    
+
             const starElement = document.createElement("div");
             starElement.style.position = "absolute";
             starElement.style.top = y + "px";
@@ -29,75 +27,46 @@ document.addEventListener('DOMContentLoaded', function() {
             starElement.style.height = starSize + "px";
             starElement.style.backgroundColor = "white";
             starElement.style.borderRadius = "50%";
-            document.body.appendChild(starElement);
+            fragment.appendChild(starElement);
         }
+        document.body.appendChild(fragment);
     }
-    
-    
-    createRandomStars();
 
     function createShootingStar() {
         const x = Math.random() * window.innerWidth;
         const y = Math.random() * window.innerHeight;
-    
+
         const shootingStar = document.createElement("div");
         shootingStar.style.left = x + "px";
         shootingStar.style.top = y + "px";
         shootingStar.classList.add("shooting-star");
-    
+
         document.body.appendChild(shootingStar);
-    
+
         setTimeout(() => {
             document.body.removeChild(shootingStar);
-        }, 1500); // remove the shooting star after the animation completes
-    
-        setTimeout(createShootingStar, Math.random() * 5000 + 2000);
+        }, 1500);
+
+        setTimeout(createShootingStar, Math.random() * 10000 + 5000);
     }
-    
-    createShootingStar();
-    
-    
+
     function updateTime(timeZone, elementId) {
         const options = { timeZone: timeZone, hour: 'numeric', minute: 'numeric', second: 'numeric' };
         const currentTime = new Date().toLocaleTimeString('en-US', options);
         document.getElementById(elementId).textContent = currentTime;
     }
 
-    function updateNamesAndTimezones() {
-        const nickoNameEl = document.getElementById('nicko-name');
-        const sunyoungNameEl = document.getElementById('sunyoung-name');
-
-        nickoNameEl.addEventListener('mouseover', () => {
-            nickoNameEl.querySelector('.name').style.display = 'none';
-            nickoNameEl.querySelector('.timezone').style.display = 'block';
-        });
-
-        nickoNameEl.addEventListener('mouseout', () => {
-            nickoNameEl.querySelector('.name').style.display = 'block';
-            nickoNameEl.querySelector('.timezone').style.display = 'none';
-        });
-
-        sunyoungNameEl.addEventListener('mouseover', () => {
-            sunyoungNameEl.querySelector('.name').style.display = 'none';
-            sunyoungNameEl.querySelector('.timezone').style.display = 'block';
-        });
-
-        sunyoungNameEl.addEventListener('mouseout', () => {
-            sunyoungNameEl.querySelector('.name').style.display = 'block';
-            sunyoungNameEl.querySelector('.timezone').style.display = 'none';
-
-
-        });
-
-        document.querySelectorAll('.initial-name').forEach(el => {
+    function colorNames() {
+        const names = document.querySelectorAll('.initial-name');
+        names.forEach(el => {
             if (el.textContent.includes('Nicko')) {
                 el.style.color = 'blue';
             } else if (el.textContent.includes('Sunyoung')) {
                 el.style.color = 'pink';
             }
         });
-        
     }
+
     function animateCelestialBodies() {
         moonDegree += 0.012;
         sunDegree += 0.0023;
@@ -107,16 +76,18 @@ document.addEventListener('DOMContentLoaded', function() {
         sun.style.transform = `translateY(-50%) rotate(${sunDegree}deg) translateX(350px) rotate(-${sunDegree}deg)`;
         earth.style.backgroundImage = `radial-gradient(circle at ${earthRotation%100}% 20%, #fff, #0077D9 20%)`;
 
-        requestAnimationFrame(animateCelestialBodies); 
+        requestAnimationFrame(animateCelestialBodies);
     }
 
     updateTime('America/Los_Angeles', 'nicko-time');
     updateTime('Asia/Seoul', 'sunyoung-time');
     setInterval(() => {
-        updateTime('America/Los_Angeles', 'nicko-time');  // Corrected timezone
-        updateTime('Asia/Seoul', 'sunyoung-time');         // Corrected timezone
+        updateTime('America/Los_Angeles', 'nicko-time');
+        updateTime('Asia/Seoul', 'sunyoung-time');
     }, 1000);
 
-    updateNamesAndTimezones();
-    requestAnimationFrame(animateCelestialBodies); 
+    createRandomStars();
+    createShootingStar();
+    colorNames();
+    requestAnimationFrame(animateCelestialBodies);
 });
