f0fe97c31700aa6f058fd745b672d4e00bb09d9d
Nicko <nickolearns2code@gmail.com>
2023-08-21 22:48:38 -0700

    Refactor celestial animation JS for clarity and maintainability
    - Introduced clear constant declarations.
    - Simplified speed calculation operations.
    - Enhanced code readability with added comments.
    - Removed unused sun animation references.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const moon = document.querySelector('.moon');
-    const sun = document.querySelector('.sun');
     const earth = document.querySelector('.earth');
+    
+    // Constants
+    const STAR_COUNT = 200;
+    const STAR_SIZE = 1;
 
-    let earthRotationSpeed = 360 / (24 * 60 * 60); 
-    let moonOrbitSpeed = 360 / (27.3 * 24 * 60 * 60); 
-    let sunOrbitSpeed = 360 / (365.25 * 24 * 60 * 60); 
+    // Speed multipliers
+    const EARTH_ROTATION_MULTIPLIER = 10;
+    const MOON_ORBIT_MULTIPLIER = 10;
 
-    earthRotationSpeed *= 10;
-    moonOrbitSpeed *= 10;
-    sunOrbitSpeed *= 10;
+    // Calculate the rotation and orbit speeds
+    let earthRotationSpeed = (360 / (24 * 60 * 60)) * EARTH_ROTATION_MULTIPLIER;
+    let moonOrbitSpeed = (360 / (27.3 * 24 * 60 * 60)) * MOON_ORBIT_MULTIPLIER;
 
     let moonDegree = 0;
-    let sunDegree = 0;
     let earthRotation = 0;
 
     function createRandomStars() {
-        const starCount = 200;
-        const starSize = 1;
         const earthRect = earth.getBoundingClientRect();
         const fragment = document.createDocumentFragment();
         let createdStars = 0;
 
-        while (createdStars < starCount) {
+        while (createdStars < STAR_COUNT) {
             const x = Math.random() * window.innerWidth;
             const y = Math.random() * window.innerHeight;
 
@@ -34,8 +34,8 @@ document.addEventListener('DOMContentLoaded', function() {
             starElement.style.position = "absolute";
             starElement.style.top = y + "px";
             starElement.style.left = x + "px";
-            starElement.style.width = starSize + "px";
-            starElement.style.height = starSize + "px";
+            starElement.style.width = STAR_SIZE + "px";
+            starElement.style.height = STAR_SIZE + "px";
             starElement.style.backgroundColor = "white";
             starElement.style.borderRadius = "50%";
             fragment.appendChild(starElement);
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.removeChild(shootingStar);
         }, 1500);
 
+        // Schedule next shooting star
         setTimeout(createShootingStar, Math.random() * 10000 + 5000);
     }
 
@@ -71,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateBothTimes() {
-        updateTime('America/Los_Angeles', 'nicko-time');
+        updateTime('America/New_York', 'nicko-time');
         updateTime('Asia/Seoul', 'sunyoung-time');
     }
 
@@ -88,7 +89,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function animateCelestialBodies() {
         moonDegree += moonOrbitSpeed;
-        sunDegree += sunOrbitSpeed;
         earthRotation += earthRotationSpeed;
 
         earth.style.transform = `translateY(-50%) rotate(${earthRotation}deg) translateX(250px) rotate(-${earthRotation}deg)`;
@@ -106,11 +106,10 @@ document.addEventListener('DOMContentLoaded', function() {
     colorNames();
     requestAnimationFrame(animateCelestialBodies);
 
-    // Handle the resize event
+    // Recreate stars on window resize
     window.addEventListener('resize', function() {
         const stars = document.querySelectorAll('.star');
         stars.forEach(star => star.remove());
-
         createRandomStars();
     });
 });
